ea2fd52d265c49a05c76f64133c44967a7db28db
Nishant <nishant.sharma.cd.ece22@itbhu.ac.in>
2024-07-05 19:29:06 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+
+
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '2b54f63708msh43d03284102ca83p1932f0jsna6d609c5bfe3',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '2b54f63708msh43d03284102ca83p1932f0jsna6d609c5bfe3',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
